989ba857d6d3a8e31ec2932d991955e790c2da01
viveknamaye <71597212+viveknamaye@users.noreply.github.com>
2021-07-16 00:40:18 +0530

    :sparkles: add template.cpp

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//#define endl "\n"
+#define pb push_back
+#define ll long long int
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define rf(i, a, b) for(int i=a;i>=b;i--)
+
+#define shov(v) for(auto i: v) cout<<i<<" ";cout<<endl;
+#define shom(m) for(auto i: m) cout<<i<<" ";cout<<endl;
+#define test(t) int t;cin>>t;while(t--)
+#define __lcm(a, b) (((a) * (b)) / __gcd((a) , (b)))
+
+#define vi vector<int>
+#define vll vector<long long int>
+#define vs vector<string>
+#define mii map<int, int>
+#define si set<int>
+
+void solve() {
+    //Write Code Here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    test(t) solve();
+}
